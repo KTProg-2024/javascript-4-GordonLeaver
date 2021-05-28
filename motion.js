b568f84a065a806b1e576c2ce92e4cd76d498e93
Gordon Leaver <>
2021-05-28 13:13:27 +0000

    Completed.

--- motion.js
+++ motion.js
@@ -13,3 +13,47 @@ function myMove() {
 		  clearInterval(id);
   }
 }
+
+function up() {
+  var elem = document.getElementById("animate");    
+  var id = setInterval(frame, 5);
+  function frame() {
+	  posTop--;
+	  elem.style.top = posTop + "px"; 
+	  if ((posTop == 0))
+		  clearInterval(id);
+  }
+}
+
+function left() {
+  var elem = document.getElementById("animate");    
+  var id = setInterval(frame, 5);
+  function frame() {
+	  posLeft--;
+	  elem.style.left = posLeft + "px"; 
+	  if ((posLeft == 0))
+		  clearInterval(id);
+  }
+}
+
+function right() {
+  var elem = document.getElementById("animate");    
+  var id = setInterval(frame, 5);
+  function frame() {
+	  posLeft++;
+	  elem.style.left = posLeft + "px"; 
+	  if ((posLeft == 350))
+		  clearInterval(id);
+  }
+}
+
+function down() {
+  var elem = document.getElementById("animate");    
+  var id = setInterval(frame, 5);
+  function frame() {
+	  posTop++;
+	  elem.style.top = posTop + "px"; 
+	  if ((posTop == 350))
+		  clearInterval(id);
+  }
+}
