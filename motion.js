6c4a1b4f56b55b23e1e3c598a03199fc57fd6681
Gordon Leaver <>
2021-05-28 13:49:31 +0000

    Added Debounce.

--- motion.js
+++ motion.js
@@ -1,59 +1,90 @@
 var posTop = 0;
 var posLeft = 0;
+var debounce = false
+
 
 function myMove() {
-  var elem = document.getElementById("animate");    
-  var id = setInterval(frame, 5);
-  function frame() {
-	  posTop++;
-	  posLeft++;
-	  elem.style.top = posTop + "px"; 
-	  elem.style.left = posLeft + "px"; 
-	  if ((posTop == 350) && (posLeft == 350))
-		  clearInterval(id);
+  if (debounce == false) {
+    debounce = true
+    console.log(debounce)
+    var elem = document.getElementById("animate");    
+    elem.style.top = 0 + "px"
+	  elem.style.left = 0 + "px"
+    var id = setInterval(frame, 5);
+    function frame() {
+      console.log(debounce)
+	   posTop++;
+	    posLeft++;
+	    elem.style.top = posTop + "px"; 
+	    elem.style.left = posLeft + "px"; 
+	    if ((posTop == 350) && (posLeft == 350)) {
+		    clearInterval(id);
+        debounce = false
+      }
+    }
   }
 }
 
 function up() {
-  var elem = document.getElementById("animate");    
-  var id = setInterval(frame, 5);
-  function frame() {
-	  posTop--;
-	  elem.style.top = posTop + "px"; 
-	  if ((posTop == 0))
-		  clearInterval(id);
+  if (debounce == false) {
+    debounce = true
+    var elem = document.getElementById("animate");    
+    var id = setInterval(frame, 5);
+    function frame() {
+  	  posTop--;
+  	  elem.style.top = posTop + "px"; 
+  	  if ((posTop == 0)) {
+	  	  clearInterval(id);
+        debounce = false
+      }
+    }
   }
 }
 
 function left() {
-  var elem = document.getElementById("animate");    
-  var id = setInterval(frame, 5);
-  function frame() {
-	  posLeft--;
-	  elem.style.left = posLeft + "px"; 
-	  if ((posLeft == 0))
-		  clearInterval(id);
+  if (debounce == false) {
+    debounce = true
+    var elem = document.getElementById("animate");    
+    var id = setInterval(frame, 5);
+    function frame() {
+  	  posLeft--;
+	    elem.style.left = posLeft + "px"; 
+	    if ((posLeft == 0)) {
+	  	  clearInterval(id);
+        debounce = false
+      }
+   }
   }
 }
 
 function right() {
-  var elem = document.getElementById("animate");    
-  var id = setInterval(frame, 5);
-  function frame() {
-	  posLeft++;
-	  elem.style.left = posLeft + "px"; 
-	  if ((posLeft == 350))
-		  clearInterval(id);
+  if (debounce == false) {
+    debounce = true
+    var elem = document.getElementById("animate");    
+    var id = setInterval(frame, 5);
+    function frame() {
+	    posLeft++;
+	    elem.style.left = posLeft + "px"; 
+	    if ((posLeft == 350)) {
+		    clearInterval(id);
+        debounce = false
+      }
+    }
   }
 }
 
 function down() {
-  var elem = document.getElementById("animate");    
-  var id = setInterval(frame, 5);
-  function frame() {
-	  posTop++;
-	  elem.style.top = posTop + "px"; 
-	  if ((posTop == 350))
-		  clearInterval(id);
+  if (debounce == false) {
+    debounce = true
+   var elem = document.getElementById("animate");    
+   var id = setInterval(frame, 5);
+    function frame() {
+  	  posTop++;
+  	  elem.style.top = posTop + "px"; 
+	    if ((posTop == 350)) {
+  		  clearInterval(id);
+        debounce = false
+      }
+    }
   }
 }
